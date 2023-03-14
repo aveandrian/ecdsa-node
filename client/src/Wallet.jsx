6e5a24ad7793677f2e84d93c35a61abacc5cd59d
vtb4036781 <andrianovav@dev.vtb.ru>
2023-03-14 15:12:56 +0100

    Added ECDSA to transfer

--- client/src/Wallet.jsx
+++ client/src/Wallet.jsx
@@ -1,7 +1,7 @@
 import server from "./server";
 
-function Wallet({ address, setAddress, balance, setBalance }) {
-  async function onChange(evt) {
+function Wallet({ address, setAddress, balance, setBalance, privateKey, setPrivateKey }) {
+  async function onChangeAddress(evt) {
     const address = evt.target.value;
     setAddress(address);
     if (address) {
@@ -14,13 +14,24 @@ function Wallet({ address, setAddress, balance, setBalance }) {
     }
   }
 
+  async function onChangePrivateKey(evt) {
+    const privateKey = evt.target.value;
+    setPrivateKey(privateKey);
+    
+  }
+
   return (
     <div className="container wallet">
       <h1>Your Wallet</h1>
 
       <label>
         Wallet Address
-        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChange}></input>
+        <input placeholder="Type an address, for example: 0x1" value={address} onChange={onChangeAddress}></input>
+      </label>
+
+      <label>
+        Private Key
+        <input placeholder="Type a private key for your address" value={privateKey} onChange={onChangePrivateKey}></input>
       </label>
 
       <div className="balance">Balance: {balance}</div>
--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const secp = require("ethereum-cryptography/secp256k1");
+const { keccak256 } = require("ethereum-cryptography/keccak");
+const { toHex } = require("ethereum-cryptography/utils");
 const port = 3042;
+
+const address_1 = '0x9883551e7e6459a7c7e4eadc4c0f526fe43e4820';
+const address_2 = '0xa10b77d7b1eeb4ea065dfa68a8406ba010c7ddf4';
+const address_3 = '0x00faf94a2a7863c86234860e398bd64b9e5933d4';
+
+const PRIVATE_KEY_X1 = "0x14ca99ca0acf5276d6b48fc486488b9e3d78c351ee39e46a130a3046574faeef";
+const PRIVATE_KEY_X2 = "0xea862fb92ebcb989a9797fba58e353da07feba1405546c855659929351ea94e8";
+const PRIVATE_KEY_X3 = "0x4c71673533108b71ad47fb9b29966a68e1e56dcccd69ab55600eb0621410a8e4";
+
+
+function getAddress(publicKey) {
+  key = publicKey.slice(1)
+  pubKeyHash = keccak256(key)
+  return pubKeyHash.slice(pubKeyHash.length-20)
+}
 
 app.use(cors());
 app.use(express.json());
 
 const balances = {
-  "0x1": 100,
-  "0x2": 50,
-  "0x3": 75,
+  address_1: 100,
+  address_2: 50,
+  address_3: 75,
 };
 
 app.get("/balance/:address", (req, res) => {
@@ -19,18 +37,33 @@ app.get("/balance/:address", (req, res) => {
 });
 
 app.post("/send", (req, res) => {
-  const { sender, recipient, amount } = req.body;
+  const { sender, recipient, amount, privateKey } = req.body;
 
   setInitialBalance(sender);
   setInitialBalance(recipient);
-
-  if (balances[sender] < amount) {
-    res.status(400).send({ message: "Not enough funds!" });
-  } else {
-    balances[sender] -= amount;
-    balances[recipient] += amount;
-    res.send({ balance: balances[sender] });
+  let publicKey,addressFromPublicKey
+  try{
+    publicKey = secp.getPublicKey(privateKey.slice(privateKey.length-64));
+  }catch(e){
+    res.status(400).send({ message: "Unable to verify address from privateKey" });
+    return
   }
+  try {
+    addressFromPublicKey = toHex(getAddress(publicKey));
+  }catch(e){
+    res.status(400).send({ message: "Unable to verify address from privateKey" });
+    return
+  }
+
+  if(sender!='0x'+addressFromPublicKey)
+    res.status(400).send({ message: "You are not the owner of the address" });
+  else if (balances[sender] < amount) {
+      res.status(400).send({ message: "Not enough funds!" });
+    } else {
+      balances[sender] -= amount;
+      balances[recipient] += amount;
+      res.send({ balance: balances[sender] });
+      }
 });
 
 app.listen(port, () => {
